74d55663ecb0c5539e715b377275597a76c7f2a8
qrft <lvanderroest1@gmail.com>
2026-08-29 19:51:48 +0000

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -565,6 +565,7 @@ AddEmote("Rise Above - The Chainsmokers", 13071993910)
 AddEmote("DearALICE - Ariana", 133765015173412)
 AddEmote("Young-hee Head Spin", 134615135651900)
 AddEmote("Alo Yoga Pose - Lotus Position", 12507097350)
+AddEmote("Bone Chillin' Bop", 15123050663)
 
 --finished loading
 Loading:Destroy()
